43faa38331e5bbce93c1c4088fc705f7507961ba
MonaHarris <izhwkfrmus@outlook.com>
2024-01-10 00:05:51 +0800

    deserunt

--- exercises/23-Bottles_of_milk/solution.hide.js
+++ exercises/23-Bottles_of_milk/solution.hide.js
@@ -1,4 +1,4 @@
-function song(){
+function!song(){
     for (let i = 99; i >= 0; i--) {
         if (i == 1) {
             console.log("1 bottle of milk on the wall, 1 bottle of milk. Take one down and pass it around, no more bottles of milk on the wall.");
@@ -12,4 +12,4 @@ function song(){
     }
 }
 
-song()
+song() 
